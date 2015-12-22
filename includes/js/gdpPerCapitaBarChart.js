4ae784ed34bd107f7a689ed14975c2f81b8be55a
venkatkrishnan <venkatkrishnan.u@hotmail.com>
2015-12-22 18:01:44 +0530

    customized html

--- includes/js/gdpPerCapitaBarChart.js
+++ includes/js/gdpPerCapitaBarChart.js
@@ -20,7 +20,7 @@ var yAxis = d3.svg.axis()
     .orient("left")
     .tickFormat(d3.format("1s"));
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#barChart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
