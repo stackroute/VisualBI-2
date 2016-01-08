fb9ea395f02a8bf93ca6e62b6f3a9d0fa8e5c83b
Ashok Kumar <ashokruhela@gmail.com>
2016-01-08 14:59:40 +0530

    added method to user schema

--- model/user.js
+++ model/user.js
@@ -9,7 +9,7 @@ var UserSchema = mongoose.Schema({
       showLegend: Boolean
    }],
    dashboards: [{
-      sharedWith: [String],
+      sharedWith: [mongoose.Schema.Types.Mixed],
       tabs: [{tabTitle: String,
          tabId: String,
          rows: [{
@@ -21,4 +21,25 @@ var UserSchema = mongoose.Schema({
    }]
 }, {strict: false});
 
+UserSchema.methods.getDashboard = function getDashboard(emailId, callback) {
+   console.log('insite dashboard');
+   Users.find({
+      'emailId': emailId
+   })
+   .select({
+      'dashboards.tabs.tabTitle': 1,
+      'dashboards.tabs.tabId': 1,
+      '_id': 0
+   })
+   .exec(function(err, data){
+      for(i in data){
+         var ds = data[i].dashboards;
+         for(i in ds){
+            dashboards = ds[i].tabs;
+            console.log(dashboards);
+            callback(dashboards);
+         }
+      }
+   });
+}
 module.exports = mongoose.model("User", UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,18 @@
-
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var utils = require('./utils');
-var Users = require('../model/user');
+var User = require('../model/user');
 
 // Inedex page
 router.get('/', function(req, res, next) {
 
-   console.log('inside index');
-   var cursor = Users.find(function(err, users){
-      console.log(users);
+   User.getDashboard('ashok.kumar6@wipro.com', function(data){
+      res.render('index', {
+         dashboards: data
+      });
    });
-
-   // cursor.each(function(err, doc) {
-   //    console.log(doc);
-   // })
-
-   var data = utils.readFile(path.join(__dirname, '../public/data/dashboards.json'))
-   var dashboards = JSON.parse(data);
-   res.render('index', {
-      dashboards: dashboards
-   });
-
 });
 
 module.exports = router;
