07692ee83b6bfe3fd4693137fd6b746b9b7e0fbb
Ashok Kumar <ashokruhela@gmail.com>
2016-02-10 23:45:02 +0530

    license information added

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,26 @@
-(function(){
-//    alert('hello angular');
-    angular.module('vbiApp', ['ngRoute', 'ui.bootstrap', 'ngCookies','angular-timeline','ngTagsInput', 'ngDragDrop', 'ngMaterial', 'ngFileUpload']);
-
-})();
+/*
+    * Copyright 2016 NIIT Ltd, Wipro Ltd.
+    *
+    * Licensed under the Apache License, Version 2.0 (the "License");
+    * you may not use this file except in compliance with the License.
+    * You may obtain a copy of the License at
+    *
+    *    http://www.apache.org/licenses/LICENSE-2.0
+    *
+    * Unless required by applicable law or agreed to in writing, software
+    * distributed under the License is distributed on an "AS IS" BASIS,
+    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    * See the License for the specific language governing permissions and
+    * limitations under the License.
+    *
+    * Contributors:
+    *
+    * 1. Ashok Kumar
+    * 2. Partha Mukharjee
+    * 3. Nabila Rafi
+    * 4. Venkatakrishnan
+    * 5. Arun Karthic
+    * 6. Hari Prasad Timmapathini
+	 * 7. Yogesh Goyal
+ */
+angular.module('vbiApp', ['ngRoute', 'ui.bootstrap', 'ngCookies','angular-timeline','ngTagsInput', 'ngDragDrop', 'ngMaterial', 'ngFileUpload']);
