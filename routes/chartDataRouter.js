87672917b5ba6a28e86821870aeaeed79ecb812d
Ashok Kumar <ashokruhela@gmail.com>
2016-02-26 15:57:56 +0530

    corrected utils reference

--- routes/chartDataRouter.js
+++ routes/chartDataRouter.js
@@ -36,7 +36,7 @@ router.get('/:chartType', function(req, res, next) {
    // picks :chartType from the URL
    var chartType = req.params.chartType;
    var filePath = path.join(__dirname, '../public/data/chartdata/' + chartType + ".json");
-      var chartJsonData = util.readFile(filePath);
+      var chartJsonData = utils.readFile(filePath);
       res.json(chartJsonData);
 });
 
